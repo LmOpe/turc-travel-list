1f608cbdb0ee1223e7c7d7a3990ea428c95135d8
LmOpe <lawalmuhammedope@gmail.com>
2024-01-27 02:52:38 +0100

    Added toggling item pack/unpack feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import { useState } from "react";
 
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: true },
-//   { id: 3, description: "Charger", quantity: 1, packed: false },
-// ];
-
 export default function App() {
   const [items, setItems] = useState([]);
 
@@ -16,12 +10,18 @@ export default function App() {
   function handleDeleteItem(id) {
     setItems((items) => items.filter((item => item.id !== id)))
   }
+
+  function handleToggleItem(id) {
+    setItems(items => items.map(item => item.id === id ? { ...item, packed: !item.packed } : item))
+  }
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
       <PackingLIst items={items}
-        onDeleteIem={handleDeleteItem} />
+        onDeleteIem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
       <Stats />
     </div>
   )
@@ -70,20 +70,28 @@ function Form({ onAddItems }) {
     </form>
   );
 }
-function PackingLIst({ items, onDeleteIem }) {
+function PackingLIst({ items, onDeleteIem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} onDeleteIem={onDeleteIem} />
+          <Item item={item} key={item.id}
+            onDeleteIem={onDeleteIem}
+            onToggleItem={onToggleItem}
+          />
         ))}
       </ul>
     </div>
   );
 }
-function Item({ item, onDeleteIem }) {
+function Item({ item, onDeleteIem, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
